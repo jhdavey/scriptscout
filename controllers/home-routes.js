5fec8081a289d3e47d58d87e5f38540404876c7a
jhdavey <jhdavey5182@gmail.com>
2023-06-06 21:36:18 -0400

    fixed profile route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -45,7 +45,8 @@ router.get('/profile', withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
-      attributes: ['id'],
+      attributes: ['id', 'name', 'jobTitle', 'location', 'aboutMe'],
+      exclude: ['password'],
     });
     const user = userData.get({ plain: true });
     res.render('profile', {
